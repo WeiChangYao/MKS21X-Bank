c49b272b640e0137ada4d90366dafe54c517a900
weichanhyao <wyao1@stuy.edu>
2018-10-04 01:17:33 -0400

    made the driver test all the stuff. Yay im done with homeworkgit add .git add .

--- Driver.java
+++ Driver.java
@@ -3,5 +3,21 @@ public class Driver{
 		BankAccount a = new BankAccount(1000000.1, 210956108, "weichang1");
 		System.out.println(a.getAccountID() + " has " + a.getBalance() + " dollars");
 		//please print "210956108 has 1000000.1 dollars
+		if(a.withdraw(1000) ){
+            		System.out.println("Withdrawal success!");
+       		}else{
+            		System.out.println("Withdrawal failure");
+       		}
+		if(a.deposit(-1000000) ){
+            		System.out.println("Deposit success!");
+       		}else{
+            		System.out.println("Deposit failure");
+       		}
+		if(a.deposit(1000000) ){
+            		System.out.println("Deposit success!");
+       		}else{
+            		System.out.println("Deposit failure");
+       		}
+		System.out.println(a.getAccountID() + " has " + a.getBalance() + " dollars");
 	}
 }
